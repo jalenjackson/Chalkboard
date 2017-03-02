21f3d41b3f95d0c7fb850966540ffe70073bfa1d
Jalen Jackson <jalenjack201@hotmail.com>
2017-03-02 02:49:47 -0500

    Commenters can only delete their comments, and not anyone elses

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
-  before_action :find_comment, only: [:destroy, :edit, :update]
+  before_action :find_post
+  before_action :find_comment, only: [:destroy, :edit, :update, :comment_owner ]
+  before_action :comment_owner, only: [:destroy, :edit , :update]
 
   def create
-    @post = Pin.find(params[:pin_id])
     @comment = @post.comments.create(params[:comment].permit(:content))
     @comment.user_id = current_user.id
     @comment.save
@@ -13,15 +14,37 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment.destroy
+    redirect_to pin_path(@post)
+  end
 
+  def edit
+
+  end
+
+  def update
+    if @comment.update(params[:comment].permit(:content))
+      redirect_to pin_path(@post)
+    else
+      render 'edit'
+    end
+  end
 
 
   private
 
   def find_post
+    @post = Pin.find(params[:pin_id])
   end
-
   def find_comment
     @comment = @post.comments.find(params[:id])
+  end
+
+  def comment_owner
+    unless current_user.id == @comment.user_id
+      flash[:notice] = "No"
+      redirect_to @post
+    end
   end
 end
